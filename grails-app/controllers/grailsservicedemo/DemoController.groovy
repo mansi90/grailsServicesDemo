76d8618c2790a8c5d21b07df0f38f5207d06b4ba
mansiarora <mansi@intelligrape.com>
2015-06-08 12:13:16 +0530

    Added logic to check transactional behaviour;

--- grails-app/controllers/grailsservicedemo/DemoController.groovy
+++ grails-app/controllers/grailsservicedemo/DemoController.groovy
@@ -1,6 +1,21 @@
 package grailsservicedemo
 
 class DemoController {
+    def demoService
 
-    def index() { }
+    def index() {
+        Sample sample = createOneObject()
+        printValues(sample)
+        demoService.fiddle(sample.id, 100)
+        printValues(sample)
+    }
+
+    def createOneObject() {
+        new Sample(name: 'TEST1', content: 'ABCDE').save(flush: true)
+    }
+
+    def printValues(Sample sample){
+        println("Total Objects > "+Sample.count())
+        println("Object 1 > Content > "+sample.content)
+    }
 }
--- grails-app/services/grailsservicedemo/DemoService.groovy
+++ grails-app/services/grailsservicedemo/DemoService.groovy
@@ -5,7 +5,22 @@ import grails.transaction.Transactional
 @Transactional
 class DemoService {
 
-    def serviceMethod() {
+    @Transactional
+    def fiddle(Long id1, Long id2) {
+        println('In Demo Service')
+        def object1 = Sample.findById(id1)
+        println(object1)
+        object1.content = "A change"
+        object1.save(flush: true) // will cause hibernate to perform the update statements
 
+        def object2 = Sample.findById(id2) ?: new Sample()
+        println(object2)
+
+        object2.name = "TEST2"
+
+        if (!object2.save(flush: true)) { // will also write to the db
+            // This will throw an runtime exception and the transaction will be rolled back (i.e Object1's content property will remain unchanged.
+            throw new RuntimeException("An exception occurred!")
+        }
     }
 }
